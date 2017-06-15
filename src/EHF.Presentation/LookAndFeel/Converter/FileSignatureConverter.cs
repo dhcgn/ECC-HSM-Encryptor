ef79f5d6febfd26c4371837d2288b0a963468078
Daniel <daniel@dhcgn.de>
2017-06-15 14:02:06 +0200

    Close file signature issues
    close https://github.com/dhcgn/Encryption-Suite/issues/16
    close https://github.com/dhcgn/ECC-HSM-Encryptor/issues/10

---
+++ src/EHF.Presentation/LookAndFeel/Converter/FileSignatureConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace EccHsmEncryptor.Presentation.LookAndFeel.Converter
+{
+    public class FileSignatureConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string))
+                return "no";
+
+            var hasFileSignature = EncryptionSuite.Encryption.FileOperation.HasFileSignature((string) value);
+            return hasFileSignature ? "yes" : "no";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
